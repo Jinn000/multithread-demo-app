f7f90c0cd51169d87c357e02e35eb500eb240abc
Andrey <andrey@none.ru>
2025-07-04 17:24:00 +0300

    Initial commit

---
+++ src/main/java/ru/zav/multithread_demo_app/MultithreadDemoAppApplication.java
@@ -0,0 +1,13 @@
+package ru.zav.multithread_demo_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultithreadDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultithreadDemoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/zav/multithread_demo_app/MultithreadDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zav.multithread_demo_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultithreadDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
